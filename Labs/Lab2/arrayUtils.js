1a919106a6143560faf08084943d0ee4df0d35fb
amandal3 <aly@stevens.edu>
2019-09-12 18:31:59 -0400

    temp

--- Labs/Lab2/arrayUtils.js
+++ Labs/Lab2/arrayUtils.js
@@ -1,20 +1,42 @@
-//6 functions, each of which will pertain to arrays.
-//1. head(array)
+//Total of 6 functions, each of which will pertain to arrays.
+function checkArray(input) {
+    if (typeof input !== array) {
+        throw `${input || "provided input"} is not an array`;
+    }
+}
 
+//1. head(array)
+function head(array) {
+
+}
 
 //2. last(array)
+function last(array) {
 
+}
 
 //3. remove(array,index)
+function remove(array, index) {
 
+}
 
 //4. range(end,value)
+function range(end, value) {
 
+}
 
 
 
 //5. countElements(array)
+function countElements(array) {
 
+}
 
 
 //6. isEqual(arrayOne, arrayTwo)
+function isEqual(arrayOne, arrayTwo) {
+    if (arrayOne == arrayTwo) {
+
+    }
+
+}
