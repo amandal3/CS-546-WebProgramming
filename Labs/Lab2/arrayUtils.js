747029f15fcbb74806380f53959f8410ff53ae25
amandal3 <aly@stevens.edu>
2019-09-15 00:11:19 -0400

    head, last, remove functions

--- Labs/Lab2/arrayUtils.js
+++ Labs/Lab2/arrayUtils.js
@@ -1,42 +1,89 @@
-//Total of 6 functions, each of which will pertain to arrays.
+/*I used the following site:
+https://webbjocke.com/javascript-check-data-types/
 function checkArray(input) {
-    if (typeof input !== array) {
-        throw `${input || "provided input"} is not an array`;
+    return input && typeof input === 'object' && input.constructor === Array;
+}
+Couldn't get above to work */
+
+
+//To check if arrays exist, I used Array.isArray()
+//Found information from: https://www.geeksforgeeks.org/javascript-array-isarray-function/
+
+//1. head(array)
+head = function head(array) {
+    if (array !== undefined && Array.isArray(array) && array.length !== 0) {
+        return array[0];
+    } else if (array == undefined) {
+        throw "Undefined array.";
+    } else {
+        throw "Array is empty.";
     }
 }
 
-//1. head(array)
-function head(array) {
-
-}
-
 //2. last(array)
-function last(array) {
-
+last = function last(inputarray) {
+    if (inputarray !== undefined && Array.isArray(inputarray) && inputarray.length !== 0) {
+        return inputarray[inputarray.length - 1];
+    } else if (inputarray == undefined) {
+        throw "Undefined array.";
+    } else {
+        throw "Array is empty.";
+    }
 }
 
 //3. remove(array,index)
-function remove(array, index) {
-
+remove = function remove(array, index) {
+    if (array !== undefined && Array.isArray(array) && array.length !== 0) {
+        if (index < array.length) {
+            //delete array[index]; does not work as intended
+            //instead used splice: https://love2dev.com/blog/javascript-remove-from-array/
+            for (i = 0; i < array.length; i++) {
+                array.splice(index, i);
+            }
+            return array;
+        } else {
+            throw "Index not in array";
+        }
+    } else if (array === undefined) {
+        throw "Undefined array.";
+    } else {
+        throw "Array is empty.";
+    }
 }
 
 //4. range(end,value)
-function range(end, value) {
+range = function range(end, value) {
 
 }
 
 
 
 //5. countElements(array)
-function countElements(array) {
+countElements = function countElements(array) {
 
 }
 
 
 //6. isEqual(arrayOne, arrayTwo)
-function isEqual(arrayOne, arrayTwo) {
-    if (arrayOne == arrayTwo) {
-
-    }
+isEqual = function isEqual(arrayOne, arrayTwo) {
 
 }
+
+module.exports = {
+    head,
+    last,
+    remove,
+    range,
+    countElements,
+    isEqual
+    // QUESTION: why does the head function not work inside here? 
+    /*head: (array) => {
+        if (array !== undefined && Array.isArray(array) && array.length !== 0) {
+            return array[0];
+        } else if (array === undefined) {
+            throw "Undefined array.";
+        } else {
+            throw "Array is empty."
+        }
+    },*/
+};
--- Labs/Lab2/index.js
+++ Labs/Lab2/index.js
@@ -1,21 +1,140 @@
 /*In your index.js file, you must import four functions from any 
 of your modules and create a passing and failing test case for each.
-
-For example:
-
-// Head Tests
-try {
-   // Should Pass
-   const headOne = head([2, 3, 4]);
-   console.log('head passed successfully');
-} catch (e) {
-   console.error('head failed test case');
-}
-try {
-   // Should Fail
-   const headTwo = head(1234);
-   console.error('head did not error');
-} catch (e) {
-   console.log('head failed successfully');
-}
 */
+const arrayUtils = require("./arrayUtils");
+
+//----------------------------------------------------------------------------
+// Head Tests
+console.log('-----------------------------------------');
+console.log('Here we will be doing HEAD tests: \n');
+
+try {
+    // Should Pass
+    console.log('head passed successfully\tReturns: ' + arrayUtils.head([2, 3, 4]));
+} catch (e) {
+    console.error('head failed test case');
+}
+try {
+    // Should Fail
+    console.error('head did not error\tReturns: ' + arrayUtils.head(1234));
+} catch (e) {
+    console.log('head failed successfully');
+}
+try {
+    // Should Fail
+    console.error('head did not error\tReturns: ' + arrayUtils.head([]));
+} catch (e) {
+    console.log('head failed successfully');
+}
+try {
+    // Should Fail
+    console.error('head did not error\tReturns: ' + arrayUtils.head("banana"));
+} catch (e) {
+    console.log('head failed successfully');
+}
+try {
+    // Should Fail
+    console.error('head did not error\tReturns: ' + arrayUtils.head());
+} catch (e) {
+    console.log('head failed successfully');
+}
+try {
+    // Should Pass
+    console.error('head passed successfully\tReturns: ' + arrayUtils.head([1, 2, 3]));
+} catch (e) {
+    console.log('head failed test case');
+}
+console.log('-----------------------------------------\n');
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+//Last Tests
+console.log('-----------------------------------------');
+console.log('Here we will be doing LAST tests: \n');
+
+try {
+    // Should Pass
+    console.log('last passed successfully\tReturns: ' + arrayUtils.last([2, 3, 4, 5]));
+} catch (e) {
+    console.error('last failed test case');
+}
+try {
+    // Should Fail
+    console.error('last did not error\tReturns: ', arrayUtils.last(1234));
+} catch (e) {
+    console.log('last failed successfully');
+}
+try {
+    // Should Fail
+    console.error('last did not error\tReturns: ' + arrayUtils.last([]));
+} catch (e) {
+    console.log('last failed successfully');
+}
+try {
+    //Should Fail
+    console.error('last did not error\tReturns: ' + arrayUtils.last("banana"));
+} catch (e) {
+    console.log('last failed successfully');
+}
+try {
+    // Should Fail
+    console.error('last did not error\tReturns: ' + arrayUtils.last());
+} catch (e) {
+    console.log('last failed successfully');
+}
+try {
+    // Should Pass
+    console.error('last passed successfully\tReturns: ' + arrayUtils.last([1, 2, 3]));
+} catch (e) {
+    console.log('last failed test case');
+}
+console.log('-----------------------------------------\n');
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+// Remove Tests
+console.log('-----------------------------------------');
+console.log('Here we will be doing REMOVE tests: \n');
+
+try {
+    // Should Pass
+    console.log('remove passed successfully\tReturns: [' + arrayUtils.remove([5, 6, 7], 1) + ']');
+} catch (e) {
+    console.error('remove failed test case');
+}
+try {
+    // Should Fail
+    console.error('remove did not error\tReturns: [', arrayUtils.remove(1234) + ']');
+} catch (e) {
+    console.log('remove failed successfully');
+}
+try {
+    // Should Fail
+    console.error('remove did not error\tReturns: [' + arrayUtils.remove([]) + ']');
+} catch (e) {
+    console.log('remove failed successfully');
+}
+try {
+    //Should Fail
+    console.error('remove did not error\tReturns: [' + arrayUtils.remove("banana") + ']');
+} catch (e) {
+    console.log('remove failed successfully');
+}
+try {
+    // Should Fail
+    console.error('remove did not error\tReturns: [' + arrayUtils.remove() + ']');
+} catch (e) {
+    console.log('remove failed successfully');
+}
+try {
+    // Should Pass
+    console.error('remove passed successfully\tReturns: [' + arrayUtils.remove([1, 2, 3], 1) + ']');
+} catch (e) {
+    console.log('remove failed test case');
+}
+try {
+    // Should Fail
+    console.error('remove did not error\tReturns: [' + arrayUtils.remove([1, 2, 3], 3) + ']');
+} catch (e) {
+    console.log('remove failed successfully');
+}
+console.log('-----------------------------------------\n');
+//----------------------------------------------------------------------------
