a1fcf024f3eb22ce2c091deeee17480fc36b6a15
amandal3 <aly@stevens.edu>
2019-09-29 15:43:28 -0400

    temp

--- Labs/Lab4/animals.js
+++ Labs/Lab4/animals.js
@@ -1,13 +1,16 @@
-const mongoCollections = require('./mongoCollections');
-const animals = mongoCollections.animals;
+const mongoCollection = require('./mongoCollection');
+const animals = mongoCollection.animals;
+const ObjectId = require('mongodb').ObjectID;
 
 //from lecture 4: addDog
 const create = async function create(name, animalType) {
     if (!name || name !== 'string') throw 'You must provide a valid name for your animal';
-    if (!animalType || animalType !== 'string') throw 'You must provide an valid animalType';
+    if (typeof name !== 'string') throw `${name || 'first variable'} is not a string`;
+    if (!animalType) throw 'You must provide an valid animalType';
+    if (typeof animalType !== 'string') throw `${animalType || 'second variable'} is not a string`;
     if (animalType.length === 0) throw 'You must provide at least one type of animal.';
-    const animalCollection = await animals();
 
+    const animalCollection = await animals();
     let newAnimal = {
         name: name,
         animalType: animalType
@@ -17,7 +20,6 @@ const create = async function create(name, animalType) {
     if (insertInfo.insertedCount === 0) throw 'Could not add animal';
 
     const newId = insertInfo.insertedId;
-
     const farm = await this.get(newId);
     return farm;
 }
@@ -33,9 +35,12 @@ const getAll = async function getAll() {
 //from lecture 4: getDogById
 const get = async function get(id) {
     if (!id) throw 'You must provide an id to search for';
+    if (typeof id !== 'string') throw `${id || 'variable entered'} is not a string`;
 
+    const objectConv = ObjectID(id);
     const animalCollection = await animals();
-    const farmAnimal = await animalCollection.findOne({ _id: id });
+    const farmAnimal = await animalCollection.findOne({ _id: objectConv });
+
     if (farmAnimal === null) throw 'No animal with that id';
 
     return farmAnimal;
@@ -44,9 +49,11 @@ const get = async function get(id) {
 //from lecture 4: removeDog
 const remove = async function remove(id) {
     if (!id) throw 'You must provide an id to search for';
+    if (typeof id !== 'string') throw `${id || 'variable entered'} is not a string`;
 
+    const objectConv = ObjectID(id);
     const animalCollection = await animals();
-    const deletionInfo = await animalCollection.removeOne({ _id: id });
+    const deletionInfo = await animalCollection.removeOne({ _id: objectConv });
 
     if (deletionInfo.deletedCount === 0) {
         throw `Could not delete animal with id of ${id}`;
@@ -56,18 +63,21 @@ const remove = async function remove(id) {
 //from lecture 4: updateDog
 const rename = async function rename(id, newName) {
     if (!id) throw 'You must provide an id to search for';
-    if (!newName) throw 'You must provide a new name for your animal';
-    if (newName !== 'string') throw `${newName || 'second variable'} is not a string`;
+    if (typeof id !== 'string') throw `${id || 'first variable'} is not a string`;
+    if (!newName) throw 'You must provide a valid newName for your animal';
+    if (typeof newName !== 'string') throw `${newName || 'second variable'} is not a string`;
 
+    const objectConv = ObjectID(id);
     const animalCollection = await animals();
     const updatedAnimal = {
         name: newName,
         animalType: data.animalType
     };
-    const updatedInfo = await animalCollection.updateOne({ _id: id }, { $set: updatedAnimal });
+    const updatedInfo = await animalCollection.updateOne({ _id: objectConv }, { $set: updatedAnimal });
     if (updatedInfo.modifiedCount === 0) {
         throw 'could not update dog successfully';
     }
+
     return await this.get(id);
 }
 
--- Labs/Lab4/index.js
+++ Labs/Lab4/index.js
@@ -1,7 +1,40 @@
 const animals = require('./animals');
 const connection = require('./mongoConnection');
 
-async function main() {}
+
+async function main() {
+    // Create an animal named Sasha with the type of Dog
+    const Sasha = await animals.create("Sasha", "Dog");
+
+    // Log the newly created animal
+    console.log(Sasha);
+
+    // Create an animal named Lucy, with the type of Dog
+    const Lucy = await animals.create("Lucy", "Dog");
+
+    // Query all animals, and log them all
+    const allMyAnimals = await animals.getAll();
+    console.log(allMyAnimals);
+
+    // Create an animal named Duke, with a type of Walrus
+    const Duke = await animals.create("Duke", "Walrus");
+
+    // Log the newly created Duke
+    console.log(Duke);
+
+    // Rename Sasha to Sashita
+    const Sashita = await animals.rename(sasha._id, "Sashita");
+
+    // Log the newly named Sashita
+    console.log(Sashita);
+
+    // Remove Lucy
+    const removeLucy = await animals.remove(lucy._id);
+
+    // Query all animals, and log them all
+    const allAnimals = await animals.getAll();
+    console.log(allAnimals);
+}
 
 
 //call main
