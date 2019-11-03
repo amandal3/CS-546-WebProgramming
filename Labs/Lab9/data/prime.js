ba29719e6c952f22d37e44a78123343709e1c108
amandal3 <aly@stevens.edu>
2019-11-03 01:01:17 -0500

    update prime

--- Labs/Lab9/data/prime.js
+++ Labs/Lab9/data/prime.js
@@ -1,8 +1,11 @@
 let exportedMethods = {
+    // my CS501 isPrime homework 
     isPrime(number) {
         if (!number) throw 'Must provide a number'
         if (typeof number !== 'number') throw 'Must provide a number'
-
+        if (number == 1 || number == 0) {
+            return `${number} is NOT a prime number.`;
+        }
         for (let divisor = 2; divisor <= number / 2; divisor++) {
             if (number % divisor == 0) {
                 return `${number} is NOT a prime number`;
--- Labs/Lab9/public/js/form.js
+++ Labs/Lab9/public/js/form.js
@@ -1,9 +1,12 @@
 (function() {
     const calculatorMethods = {
         isPrime(number) {
+            // my CS501 isPrime homework 
             if (!number) throw 'Must provide a number'
             if (typeof number !== 'number') throw 'Must provide a number'
-
+            if (number == 1 || number == 0) {
+                return `${number} is NOT a prime number.`
+            }
             for (let divisor = 2; divisor <= number / 2; divisor++) {
                 if (number % divisor == 0) {
                     return `${number} is NOT a prime number`;
@@ -31,9 +34,24 @@
 
             // Values come from inputs as strings, no matter what
             const inputNumber = inputNumber.value;
-            const parsedFirstNumberValue = parseInt(firstNumberValue);
+            const parsedNumberValue = parseInt(inputNumber);
 
-            const result = isPrime(input);
+            const result = isPrime(parsedNumberValue);
+            console.log('RESULT is :', result);
+
+            for (let j = 0; j < result.length; j++) {
+                if (result[j].includes('NOT')) {
+                    // document.createElement('li').classList.add('not-prime');
+                    var isFalse = document.createElement('li');
+                    var temp = isFalse.classList.add('not-prime');
+                    attempts.appendChild(temp);
+                } else {
+                    // document.createElement('li').classList.add('is-prime');
+                    var isTrue = document.createElement('li')
+                    var temp = isTrue.classList.add('is-prime');
+                    attempts.appendChild(temp);
+                }
+            }
 
             resultTextElement.textContent = "The result is " + result;
             resultContainer.classList.remove("hidden");
