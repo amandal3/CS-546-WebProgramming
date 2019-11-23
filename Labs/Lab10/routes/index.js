690860bfe561acbaf28d914697dd5dcb4fe059c9
amandal3 <aly@stevens.edu>
2019-11-22 23:01:35 -0500

    something

--- Labs/Lab10/routes/index.js
+++ Labs/Lab10/routes/index.js
@@ -4,16 +4,16 @@ const privRoute = require("./private");
 const path = require("path");
 
 const constructorMethod = app => {
-	app.use("/logout", logoutRoute);
-	app.use("/login", loginRoute);
-  app.use("/private", privRoute);
-  app.get("/", (req, res) => {
-    res.render('form');
-  })
+    app.use("/logout", logoutRoute);
+    app.use("/login", loginRoute);
+    app.use("/private", privRoute);
+    app.get("/", (req, res) => {
+        res.render('login');
+    })
 
-	app.use("*", (req, res) => {
-		res.status(404).json({ error: "Not found -- Rawr Test1"});
-	});
+    app.use("*", (req, res) => {
+        res.status(404).json({ error: "Not Found: 404 Error" });
+    });
 };
 
 module.exports = constructorMethod;
--- Labs/Lab10/routes/login.js
+++ Labs/Lab10/routes/login.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const session = require("express-session")//this too
+const session = require("express-session") //this too
 const router = express.Router();
-const bcrypt = require("bcryptjs");//dont you need this
+const bcrypt = require("bcryptjs"); //dont you need this
 const data = require('../data');
 const usersData = data.users;
 const saltRounds = 16;
@@ -10,18 +10,15 @@ router.post('/', async(req, res) => {
 
     const username = req.body.username;
     const pass = req.body.password;
-    console.log('BOYS WE MADE IT')
     console.log(username)
-    console.log('RawrDood')
-    for (var i = 0; i < usersData.length; i++) {//lol we had an oopsy here
-        console.log(usersData[i].username)//used x in here when we using i
+    for (var i = 0; i < usersData.length; i++) { //lol we had an oopsy here
+        console.log(usersData[i].username) //used x in here when we using i
         if (username === usersData[i].username) {
             // another oopsy const hashP = await bcrypt.compare(password, usersData[x].hashedPassword);
             const hashP = await bcrypt.compare(pass, usersData[i].hashedPassword);
             if (hashP == true) {
-                req.session.user = usersData[i];//missed a spot here dumbo
+                req.session.user = usersData[i]; //missed a spot here dumbo
                 res.redirect("/private");
-                console.log('Rawr3')
                 return;
 
                 // Same Soft. Eng friend said this was not needed here since second error serves same functionality.
@@ -31,8 +28,7 @@ router.post('/', async(req, res) => {
             }
         }
     }
-    console.log('MonkaSMan');
-    res.render("form", { error: "Error. Try Again." });
+    res.render("login", { error: "Error. Try Again." });
 });
 
 module.exports = router;
